0ad5d2e04cb49230d2fab8442a838b21565386f8
zs1014186631 <46860480+zs1014186631@users.noreply.github.com>
2020-07-26 22:31:02 +0800

    Add files via upload

---
+++ transformnpy.py
@@ -0,0 +1,17 @@
+# import tensorflow as tf
+import numpy as np
+import glob  # 查找符合特定规则的文件路径名
+from PIL import Image
+import os
+
+Datapath = r'D:\pycharm\11111\*.jpg'
+input_raw_data = np.zeros((1400, 128, 128, 3), dtype='float32')
+i = 0
+for imageFile in glob.glob(Datapath):
+    # 打开图像并转化为数字矩阵(240x240x3)
+    print(os.path.join(Datapath, imageFile))
+    img = np.array(Image.open(imageFile))
+    input_raw_data[i] = img
+    i += 1
+
+np.save('transformnpy.npy', input_raw_data)
